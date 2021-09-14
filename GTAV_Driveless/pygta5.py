79408ba1034e27608d419d7061364088fe965aa8
Cycsics <sz597002139@outlook.com>
2021-09-14 18:16:15 +0800

    Update pygta5.py
    Add ROI

--- GTAV_Driveless/pygta5.py
+++ GTAV_Driveless/pygta5.py
@@ -2,10 +2,22 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time 
+import pyautogui
+
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
 
 def process_img(original_image):
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    vertices = np.array([[10,500], [10,300], [300,200], [500,200], [800,300], [800,500]])
+    processed_img = roi(processed_img, [vertices])
+
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 2)
+
     return processed_img
 
 last_time = time.time()
